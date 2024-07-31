89d20f510b73af7a898998665dfb16c99f33dead
Cristián Rojas <malcolmrojas.f@gmail.com>
2024-07-30 23:31:57 -0400

    auth module advance

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { InventoryModule } from './inventory/inventory.module';
 import { MicrophoneModule } from './microphone/microphone.module';
 import { SpeakerModule } from './speaker/speaker.module';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -21,6 +25,8 @@ import { SpeakerModule } from './speaker/speaker.module';
     InventoryModule,
     MicrophoneModule,
     SpeakerModule,
+    AuthModule,
+    CommonModule,
   ],
 })
 export class AppModule {}
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { IsString } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  @IsString()
+  id: string;
+
+  @Column('text', { nullable: false, unique: true })
+  email: string;
+
+  @Column('text', {
+    select: false,
+  })
+  password: string;
+
+  @Column('text')
+  fullName: string;
+
+  @Column('bool', { default: true })
+  isActive: boolean;
+
+  @Column('text', { array: true, default: ['user'] })
+  roles: string[];
+}
