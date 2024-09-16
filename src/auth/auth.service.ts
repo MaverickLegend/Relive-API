7d5fbcb752977a2cbf1adcfb3da03ed89580332f
Cristián Rojas <malcolmrojas.f@gmail.com>
2024-09-15 22:54:00 -0300

    auth mmodule advance

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,11 +22,14 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
+      // Desescructuramos el password del objeto createUserDto y copiamos todo el resto de la data
       const { password, ...userData } = createUserDto;
+      // Creamos un nuevo usuario con la data desestructurada y hasheamos la contraseña
       const user = this.userRepository.create({
         ...userData,
         password: bcrypt.hashSync(password, 10),
       });
+      // Guardamos el usuario en la base de datos
       await this.userRepository.save(user);
       delete user.password;
       return {
@@ -40,19 +43,26 @@ export class AuthService {
 
   async login(loginUserDto: LoginUserDto) {
     try {
+      // Desestructuramos el email y password del objeto loginUserDto
       const { password, email } = loginUserDto;
+      // Buscamos un usuario con el email proporcionado
       const user = await this.userRepository.findOne({
         where: { email },
+        // Solo seleccionamos el email, password e id del usuario
         select: { email: true, password: true, id: true },
       });
+      // Si no encontramos un usuario con el email proporcionado lanzamos un UnauthorizedException
       if (!user) {
         throw new UnauthorizedException('Invalid credentials');
       }
+      // Comparamos la contraseña proporcionada con la contraseña hasheada en la base de datos
       if (!bcrypt.compareSync(password, user.password)) {
         throw new UnauthorizedException('Invalid credentials');
       }
+      // Si las contraseñas coinciden, eliminamos la contraseña del objeto user y retornamos el usuario con un token JWT
       return {
         ...user,
+        // Generamos un token JWT con el id del usuario
         token: this.getJwtToken({ id: user.id }),
       };
     } catch (error) {
@@ -60,7 +70,15 @@ export class AuthService {
     }
   }
 
+  async checkAuthStatus(user: User) {
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id }),
+    };
+  }
+
   private getJwtToken(payload: JwtPayload) {
+    // Generamos un token JWT con el payload proporcionado
     const token = this.service.sign(payload);
     return token;
   }
